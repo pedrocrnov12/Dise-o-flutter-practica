cfaaee16e82bccd47d585f02fa5c70e038040c2f
pedrocrnov12 <2002pc.pc@gmail.com>
2024-05-31 21:21:07 -0600

    cambios

--- lib/title_screen/title_screen_ui.dart
+++ lib/title_screen/title_screen_ui.dart
@@ -1,6 +1,4 @@
-// Copyright 2023 The Flutter Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+
 
 import 'package:extra_alignments/extra_alignments.dart';
 import 'package:flutter/material.dart';
